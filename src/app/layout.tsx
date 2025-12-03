a585403662359c53a3bb51f8ee66b9952d4731de
Henry <hqian@irdeto.com>
2025-12-03 22:24:40 +0100

    Update metadata title for localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "消息触达计算器",
-  description: "计算推送轮次以覆盖目标用户比例",
+  title: "EMM Delivery Hit Rate Caculator",
 };
 
 export default function RootLayout({
